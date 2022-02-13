409fe5f162566867edc046dd3b9d396b7a712aaa
zhangfu <17770517023@163.com>
2022-02-13 10:47:19 +0800

    Initial commit

---
+++ src/main/java/com/bjtu/afms/AfmsApplication.java
@@ -0,0 +1,13 @@
+package com.bjtu.afms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfmsApplication.class, args);
+    }
+
+}
